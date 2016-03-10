ba5bdee0db2e5c21f618b68ff724cfdb102c2691
JeffCoutu <jeff.coutu@gmail.com>
2016-03-10 18:19:16 -0500

    Sanitized New lot Input
    lot and city cannot be blank, min lot size is 1x1

--- DronePark1/DronePark1/newlot.cpp
+++ DronePark1/DronePark1/newlot.cpp
@@ -1,25 +1,39 @@
 #include "newlot.h"
 #include "DatabaseController.h"
+#include "QMessageBox.h"
 
 NewLot::NewLot(QWidget *parent) : QDialog(parent)
 {
 	ui.setupUi(this);
 	connect(ui.numCol, SIGNAL(valueChanged(int)), this, SLOT(setColSlideValue(int)));
-	connect(ui.numRow, SIGNAL(valueChanged(int)), this, SLOT(setColSlideValue(int)));
+	connect(ui.numRow, SIGNAL(valueChanged(int)), this, SLOT(setRowSlideValue(int)));
 }
 
 void NewLot::accept()
 {
-	
+	//grab values from the new lot ui
 	int row = ui.numRow->value();
 	int col = ui.numCol->value();
 	int numSpot = row * col;
 	QString lotName = ui.lotText->text();
+	lotName = lotName.trimmed();
 	QString city = ui.cityText->text();
-	emit newLotOkSig(numSpot, row, col,lotName, city);
+	city = city.trimmed();
+	
+	// make sure city and lot input are not blank
+	if (lotName.isEmpty() || city.isEmpty())
+	{
+		QMessageBox msgBox;
+		msgBox.setText("City and Lot Name Cannot be left blank.");
+		msgBox.exec();
+	}
+	else
+	{
+		//emit signal to insert to db
+		emit newLotOkSig(numSpot, row, col, lotName, city);
 
-	QDialog::accept();
-
+		QDialog::accept();
+	}
 	return;
 }
 
